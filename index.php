4ae753641b5386b87d6a8afd6685df2c70dc532d
Thomas Galinis <tjgalinis@gmail.com>
2020-06-13 18:03:04 -0400

    feat(p8): use PHP to generate landing page

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+$projects = [
+    'title' => 'Home Page',
+    'link' => 'p1/index.html',
+], [
+    'title' => 'Web Page',
+    'link' => 'p2/index.html',
+];
+
+?><!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+    <title>Thomas Galinis' Projects</title>
+    <link rel="stylesheet" type="text/css"
+            href="vendor/bootstrap-4.5.0-dist/css/bootstrap.min.css">
+    <script type="text/javascript"
+            src="vendor/jquery-3.5.1-dist/jquery-3.5.1.min.js"></script>
+    <script type="text/javascript"
+            src="vendor/bootstrap-4.5.0-dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <div class="container my-5">
+        <h1>Thomas Galinis' Projects</h1>
+
+        <hr>
+
+        <ul>
+            <?php foreach ($projects as $project): ?>
+                <li><a href="<?= $project['url'] ?>"><?= $project['title'] ?></a></li>
+            <?php endforeach ?>
+        </ul>
+    </div>
+</body>
+</html>
+

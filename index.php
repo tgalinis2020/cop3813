3ed9a2429849387a239fc969b3266bbe046ae2fe
Thomas Galinis <tjgalinis@gmail.com>
2020-06-17 02:47:31 -0400

    feat(root): read projects from projects.json, display using cards

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php
 
-const PROJECTS = [[
-    'title' => 'Home Page',
-    'url' => 'p1/index.html',
-], [
-    'title' => 'Web Page',
-    'url' => 'p2/index.html',
-]];
+error_reporting(E_ALL);
+
+// Load content from external JSON file
+$content = json_decode(file_get_contents('projects.json'), true);
 
 ?><!DOCTYPE html>
 <html lang="en-US">
@@ -25,13 +22,45 @@ const PROJECTS = [[
     <div class="container my-5">
         <h1>Thomas Galinis' Projects</h1>
 
-        <hr>
 
-        <ul>
-            <?php foreach (PROJECTS as $p): ?>
-                <li><a href="<?= $p['url'] ?>"><?= $p['title'] ?></a></li>
-            <?php endforeach ?>
-        </ul>
+        <div class="row my-5">
+
+        <?php /*
+            Using a foreach loop to show all projects.
+
+            To put 4 cards in a row, the current row needs to
+            end and a new one begins when the current index
+            is divisible by 3. Not including the case when
+            index = 0 since I started the first row above this
+            comment.
+        */ ?>
+
+        <?php foreach ($content['projects'] as $index => $p): ?>
+
+        <?php if ($index > 0 && $index % 3 === 0): ?>
+
+        </div>
+
+        <div class="row my-5">
+
+        <?php endif ?>
+
+            <div class="col-12 col-sm-6 col-lg-4">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h5 class="card-tile"><?= sprintf("Project %d: %s", $p['id'], $p['title']) ?></h5>
+                        <p class="card-text"><?= $p['description'] ?></p>
+                    </div>
+
+                    <div class="card-footer">
+                        <a href="<?= $p['url'] ?>" class="btn btn-primary w-100">View</a>
+                    </div>
+                </div>
+            </div>
+
+        <?php endforeach ?>
+
+        </div>
     </div>
 </body>
 </html>

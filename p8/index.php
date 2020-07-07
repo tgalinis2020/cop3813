bcb8de548b68bcfaf99dad79ffb858acb31b570a
Thomas Galinis <tjgalinis@gmail.com>
2020-07-07 17:07:26 -0400

    refactor(p8): store all files in p8 directory
    rename p8.css to main.css for consistency

--- p8/index.php
+++ p8/index.php
@@ -6,26 +6,19 @@ $content = json_decode(file_get_contents(__DIR__ . '/projects.json'), true);
 ?><!DOCTYPE html>
 <html lang="en-US">
 <head>
-    <?php /*
-
-    To avoid copy+pasting project 8 into the root directory, I'll just set
-    the basename for all relative paths to the root directory itself and
-    include this file in public_html/index.php
-
-    */ ?>
-    <base href="/~tgalinis2020/">
     <meta charset="utf-8">
     <title>Thomas Galinis' Projects</title>
     <link rel="stylesheet" type="text/css"
-            href="vendor/bootstrap-4.5.0-dist/css/bootstrap.min.css">
+            href="/~tgalinis2020/vendor/bootstrap-4.5.0-dist/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css"
-            href="styles/p8.css">
+            href="/~tgalinis2020/p8/styles/main.css">
 </head>
 
 <body>
     <header>
         <img class="header__background"
-                src="assets/jquery-minified.png" alt="Picture of code">
+                src="/~tgalinis2020/p8/assets/jquery-minified.png"
+                alt="Picture of code">
 
         <div class="header__foreground container">
             <h1>Thomas Galinis' Projects</h1>
@@ -36,10 +29,7 @@ $content = json_decode(file_get_contents(__DIR__ . '/projects.json'), true);
     <main>
         <div class="container my-5">
             <div class="row">
-                <?php /* Use a foreach loop to show all projects */ ?>
-
-                <?php foreach ($content['projects'] as $index => $p): ?>
-
+                <?php foreach ($content['projects'] as $p): ?>
                     <div class="col-12 col-md-6 col-lg-4 col-xl-3 my-2">
                         <div class="card h-100">
                             <div class="card-body">
@@ -54,7 +44,6 @@ $content = json_decode(file_get_contents(__DIR__ . '/projects.json'), true);
                             </div>
                         </div>
                     </div>
-
                 <?php endforeach ?>
             </div>
         </div>

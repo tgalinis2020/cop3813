3fa7e10c95f9df716704e861fa4e163ad8a7cdb2
Thomas Galinis <tjgalinis@gmail.com>
2020-07-10 14:59:12 -0400

    refactor(p5): separate business logic from template

---
+++ p5/index.controller.php
@@ -0,0 +1,75 @@
+<?php
+
+// Some useful constants
+const B_SHOW_GENERATED_NUMBER = false;
+const MAX_ATTEMPTS = 5;
+const MIN = 1;
+const MAX = 10;
+
+// Show the user a message depending on their input
+$feedback = null;
+
+// Form validity status
+$valid = true;
+
+$inputClasses = ['form-control'];
+
+// Use PHP sessions to keep track of the random number and number of attempts
+session_start();
+
+// Reset the game if the session has not been set, if the game is over or is reset explicitly
+if (!isset($_SESSION['gameover']) || $_SESSION['gameover'] || isset($_GET['reset'])) {
+    $_SESSION['number'] = rand(MIN, MAX);
+    $_SESSION['attempts'] = 0;
+    $_SESSION['gameover'] = false;
+
+} elseif ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $guess = (int) $_POST["guess"];
+
+    if (empty($_POST['guess']) || $guess < MIN || $guess > MAX) {
+        $valid = false;
+        $inputClasses[] = 'is-invalid';
+
+    } else {
+        $valid = true;
+        $_SESSION['attempts'] += 1;
+        $randNum = $_SESSION['number'];
+
+        // Spaceship operator returns...
+        //   -1 if guess < randNum
+        //    0 if guess === randNum
+        //    1 if guess > randNum
+        $lowOrHigh = $guess <=> $randNum;
+            
+        if ($lowOrHigh === 0) {
+            $_SESSION['gameover'] = true;
+
+            $feedback = [
+                'style' => 'success',
+                'title' => 'Well done!',
+                'message' => 'You got it!',
+            ];
+
+        } else {
+            $feedback = ['style' => 'danger'];
+
+            if ($_SESSION['attempts'] === MAX_ATTEMPTS) {
+                $_SESSION['gameover'] = true;
+                $feedback['title'] = 'You are out of attempts!';
+                $feedback['message'] = sprintf(
+                    'I was thinking of %d. Better luck next time!',
+                    $_SESSION['number']
+                );
+                
+            } else {
+                if ($lowOrHigh === -1) {
+                    $feedback['title'] = 'Too low!';
+                    $feedback['message'] = 'Try a larger number.';
+                } else {
+                    $feedback['title'] = 'Too high!';
+                    $feedback['message'] = 'Try a smaller number.';
+                }
+            }
+        }
+    }
+}
--- p5/index.php
+++ p5/index.php
@@ -1,71 +1,5 @@
-<?php
-
-// Some useful constants
-const B_SHOW_GENERATED_NUMBER = false;
-const MAX_ATTEMPTS = 5;
-const MIN = 1;
-const MAX = 10;
-
-// Show the user a message depending on their input
-$feedback = null;
-
-// Form validity status
-$valid = true;
-
-// Use PHP sessions to keep track of the random number and number of attempts
-session_start();
-
-// Reset the game if the session has not been set, if the game is over or is reset explicitly.
-if (!isset($_SESSION['gameover']) || $_SESSION['gameover'] === true || isset($_GET['reset'])) {
-    $_SESSION['number'] = rand(MIN, MAX);
-    $_SESSION['attempts'] = 0;
-    $_SESSION['gameover'] = false;
-
-} elseif ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $guess = (int) $_POST["guess"];
-
-    if (empty($_POST['guess']) || $guess < MIN || $guess > MAX) {
-        $valid = false;
-    } else {
-        $valid = true;
-        $_SESSION['attempts'] += 1;
-        $randNum = $_SESSION['number'];
-        $tooLow = $guess < $randNum;
-        $tooHigh = $guess > $randNum;
-            
-        if ($tooLow || $tooHigh) {
-            $feedback = ['style' => 'danger', 'title' => '', 'message' => ''];
-
-            if ($_SESSION['attempts'] === MAX_ATTEMPTS) {
-                $_SESSION['gameover'] = true;
-                $feedback['title'] = 'You are out of attempts!';
-                $feedback['message'] = sprintf(
-                    'I was thinking of %d. Better luck next time!',
-                    $_SESSION['number']
-                );
-            } else {
-                if ($tooLow) {
-                    $feedback['title'] = 'Too low!';
-                    $feedback['message'] = 'Try a larger number.';
-                } else {
-                    $feedback['title'] = 'Too high!';
-                    $feedback['message'] = 'Try a smaller number.';
-                }
-                
-            }
-        } else {
-            $_SESSION['gameover'] = true;
-
-            $feedback = [
-                'style' => 'success',
-                'title' => 'Well done!',
-                'message' => 'You got it!',
-            ];
-        }
-    }
-}
-
-?><!DOCTYPE html>
+<?php require __DIR__ . '/index.controller.php' ?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -88,15 +22,15 @@ if (!isset($_SESSION['gameover']) || $_SESSION['gameover'] === true || isset($_G
             </ul>
 
             <?php if (!empty($feedback)): ?>
-                <div class="alert alert-<?= $feedback['style'] ?>" role="alert">
+                <div class="alert <?= 'alert-' . $feedback['style'] ?>" role="alert">
                     <strong><?= $feedback['title'] ?></strong> <?= $feedback['message'] ?>
                 </div>
             <?php endif ?>
 
             <?php if ($_SESSION['gameover']): ?>
-                <a class="btn btn-primary" href="index.php">Play Again</a>
+                <a class="btn btn-primary" href="<?= $_SERVER['PHP_SELF'] ?>">Play Again</a>
             <?php else: ?>
-                <form method="POST" action="index.php">
+                <form method="POST" action="<?= $_SERVER['PHP_SELF'] ?>">
                     <p>
                         I'm thinking of a number between <?= MIN ?> and <?= MAX ?>.
                         You have <?= MAX_ATTEMPTS - $_SESSION['attempts'] ?> attempts.
@@ -108,9 +42,9 @@ if (!isset($_SESSION['gameover']) || $_SESSION['gameover'] === true || isset($_G
 
                     <p>
                         <label for="guess">Your guess?</label>
-                        <input class="form-control<?= $valid ? '' : ' is-invalid' ?>"
+                        <input class="<?= implode(' ', $inputClasses) ?>"
                                 type="number" id="guess" name="guess"
-                                min="<?= MIN ?>" max="<?= MAX ?>" autofocus>
+                                min="<?= MIN ?>" max="<?= MAX ?>" required autofocus>
 
                         <?php if (!$valid): ?>
                             <div class="invalid-feedback">
@@ -119,7 +53,7 @@ if (!isset($_SESSION['gameover']) || $_SESSION['gameover'] === true || isset($_G
                         <?php endif ?>
                     </p>
 
-                    <input type="submit" name="submit" value="Guess" class="btn btn-primary">
+                    <input type="submit" value="Guess" class="btn btn-primary">
                     <a class="btn btn-danger" href="?reset">Reset</a>
                 </form>
             <?php endif ?>

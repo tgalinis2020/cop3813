f7bc60e79bccb19b222cf384283173e8edb1cc35
Thomas Galinis <tjgalinis@gmail.com>
2020-07-22 16:16:37 -0400

    fix(p7): update method of getting credentials

--- p7/api/dependencies/pdo_mysql.php
+++ p7/api/dependencies/pdo_mysql.php
@@ -4,13 +4,11 @@
  * Create a new instance of a PDO object using the MySQL driver.
  * 
  * Normally it's bad practice to store credentials directly within source code,
- * so I put them in a hidden file in my home directory.
+ * so I put my password in my account's .my.cnf file.
  */
-list($user, $pass) = explode(':', trim(file_get_contents('/home/tgalinis2020/.dblogin')));
-
 $dbh = new PDO(
     'mysql:host=lamp.cse.fau.edu;port=3306;charset=utf8;dbname=tgalinis2020;',
-    $user, $pass
+    'tgalinis2020'
 );
 
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);

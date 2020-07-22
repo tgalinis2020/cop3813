ffef8b8be4571ff4fb7210dc8aaf6ce089af9e7a
Thomas Galinis <tjgalinis@gmail.com>
2020-07-22 16:18:50 -0400

    fixup commit

--- p7/api/dependencies/pdo_mysql.php
+++ p7/api/dependencies/pdo_mysql.php
@@ -2,14 +2,8 @@
 
 /**
  * Create a new instance of a PDO object using the MySQL driver.
- * 
- * Normally it's bad practice to store credentials directly within source code,
- * so I put my password in my account's .my.cnf file.
  */
-$dbh = new PDO(
-    'mysql:host=lamp.cse.fau.edu;port=3306;charset=utf8;dbname=tgalinis2020;',
-    'tgalinis2020'
-);
+$dbh = new PDO('mysql:host=lamp.cse.fau.edu;port=3306;charset=utf8;dbname=tgalinis2020;');
 
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);

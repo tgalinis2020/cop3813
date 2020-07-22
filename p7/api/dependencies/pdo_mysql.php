adb4cfb5fed45d94aebd5e775d557126883d4406
Thomas Galinis <tjgalinis@gmail.com>
2020-07-22 16:22:26 -0400

    fix(p7): add mysql credentials

--- p7/api/dependencies/pdo_mysql.php
+++ p7/api/dependencies/pdo_mysql.php
@@ -2,8 +2,15 @@
 
 /**
  * Create a new instance of a PDO object using the MySQL driver.
+ *
+ * Normally it's bad practice to have credentials stored in source code,
+ * but it should be harmless given this is an academic project.
  */
-$dbh = new PDO('mysql:host=lamp.cse.fau.edu;port=3306;charset=utf8;dbname=tgalinis2020;');
+$dbh = new PDO(
+    'mysql:host=lamp.cse.fau.edu;port=3306;charset=utf8;dbname=tgalinis2020;',
+    'tgalinis2020',
+    'c2o74ZGyoo'
+);
 
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);

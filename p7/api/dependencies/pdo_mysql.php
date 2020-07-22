e76e5d45b0c91853d935a369f804e628909d3a5f
Thomas Galinis <tjgalinis@gmail.com>
2020-07-22 00:59:08 -0400

    feat(p7): initial commit of project 7

---
+++ p7/api/dependencies/pdo_mysql.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This immediately invoked function returns a new instance of a database
+ * connection via PDO using the MySQL driver.
+ * 
+ * Normally it's bad practice to store credentials directly within source code,
+ * so I put them in a hidden file in my home directory.
+ */
+return (function () {
+    list($username, $password) = explode(
+        '\n',
+        file_get_contents('/home/tgalinis2020/.useless-file-ignore-me-thanks')
+    );
+
+    $dbh = new PDO(
+        'mysql:host=lamp.cse.fau.edu;port=3306;charset=utf8;dbname=baby_names_db;',
+        $username, $password
+    );
+
+    $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    return $dbh;
+})();
---
+++ p7/scripts/client.js
@@ -0,0 +1,143 @@
+/**
+ * Frond-end component for baby names web application. Requires jQuery.
+ * 
+ * @author Thomas Galinis <tgalinis2020@fau.edu>
+ */
+$(function() {
+    'use strict'
+
+    const API_ROOT = '/~tgalinis2020/p7/api/resources'
+    const API_NAMES_ENDPOINT = `${API_ROOT}/names.php`
+    const API_VOTES_ENDPOINT = `${API_ROOT}/votes.php`
+
+    const { ajax } = window.jQuery
+    const suggestions = $('#suggestions')
+    const isMale = $('#baby_is-male')
+    const name = $('#baby_name')
+    const vote = $('#vote')
+    const boyNames = $('#top-10-boy-names')
+    const girlNames = $('#top-10-girl-names')
+
+    // A decorator function that runs the callback only when a certain
+    // amount of time has passed since the last time the function was invoked.
+    //
+    // Inspired by an implementation by David Walsh:
+    // https://davidwalsh.name/javascript-debounce-function
+    function debounced(time, callback, override) {
+        let active = false
+        let timeout = null
+
+        return function (...args) {
+            const self = this
+
+            // run the following after timeout is complete
+            function timeoutHandler() {
+                callback.apply(self, args)
+                active = false
+            }
+
+            active && window.clearTimeout(timeout)
+
+            if (override.apply(self, args)) {
+                timeoutHandler()
+            } else {
+                timeout = window.setTimeout(timeoutHandler, time)
+                active = true
+            }
+        }
+    }
+
+    const searchName = (name, gender) => ajax({
+        method: 'GET',
+        url: API_NAMES_ENDPOINT,
+        data: { name, gender },
+        success: function (res) {
+            suggestions.empty() // clear previous suggestions
+
+            // append new suggestions
+            for (const baby of res.data) {
+                const option = $('<option>')
+                option.attr('value', baby.name)
+                suggestions.append(option)
+            }
+        }
+    })
+
+    const updateLeaderboards = () => ajax({
+        method: 'GET',
+        url: API_NAMES_ENDPOINT,
+        success: function (res) {
+            const boysTbl = $('<table class="table"></table>')
+            const girlsTbl = $('<table class="table"></table>')
+            let boysCounter = 0
+            let girlsCounter = 0
+
+            // data should contain the names sorted by popularity
+            for (const { id, name, gender } of res.data) {
+                const tr = $('<tr></tr>')
+                const rank_td = $('<td></td>')
+                const name_td = $('<td></td>')
+                
+                tr.append(rank_td)
+                tr.append(name_td)
+
+                name_td.html(name)
+
+                switch (gender) {
+                    case 'M':
+                        rank_td.html(++boysCounter)
+                        boysTbl.append(tr)
+                    break;
+
+                    case 'F':
+                        rank_td.html(++girlsCounter)
+                        girlsTbl.append(tr)
+                    break;
+                }  
+            }
+
+            // remove the old tables and show new data
+            boyNames.empty()
+            girlNames.empty()
+            boyNames.append(boysTbl)
+            girlNames.append(girlsTbl)
+        }
+    })
+
+    const placeVote = () => ajax({
+        method: 'POST',
+        url: API_VOTES_ENDPOINT,
+        success: () => updateLeaderboards()
+    })
+
+    // Populate suggestions list with applicable baby names.
+    // Debounce the input stream every three-quarters of a second to prevent
+    // unecessary use of bandwidth.
+    name.keyup(debounced(
+        750, // Debounce time (in milliseconds)
+
+        // Run the following after the timeout
+        function ({ key, target }) {
+            const { value } = target
+
+            switch (key) {
+                case 'Enter':
+                    //placeVote()
+                    console.log(`Voting for ${value}`)
+                break;
+
+                default:
+                    //searchName(value, isMale.checked ? 'M' : 'F')
+                    console.log(value)
+            }
+        },
+
+        // Ignore debouncing when enter key is pressed
+        ({ key }) => key === 'Enter'
+    ))
+
+    // Place a vote (or display an error if name was not found).
+    vote.click(_ => console.log(`Voting for ${name.val()}`))
+
+    //updateLeaderboards() // show popular baby names when page loads
+})

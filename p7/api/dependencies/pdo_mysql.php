23004dc2e2197a55d42962b7216bdc4e7bf4e7d1
Thomas Galinis <tjgalinis@gmail.com>
2020-07-25 23:47:21 -0400

    refactor comment

--- p7/api/dependencies/pdo_mysql.php
+++ p7/api/dependencies/pdo_mysql.php
@@ -8,8 +8,9 @@
  * 
  * I tried several solutions but unfortunately the apache account used to run
  * PHP can't access my files outside of public_html. At first I created a
- * file in my home directory with my username and password. Tried to chmod 
- * it but I guess student accounts aren't allowed to do so.
+ * file in my home directory with my username and password. Tried to chown 
+ * it so the apache group could read from it but I guess student accounts aren't
+ * allowed to do so.
  * 
  * The next best thing, I guess, would be to store the credentials in their
  * own PHP file but ignore it from source control.

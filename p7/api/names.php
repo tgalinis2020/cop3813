7aa1c05bb97e9f3a36b764d9c84a3c1f42b47396
Thomas Galinis <tjgalinis@gmail.com>
2020-07-28 19:57:32 -0400

    fixup

--- p7/api/names.php
+++ p7/api/names.php
@@ -50,6 +50,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         $sth->execute();
 
         $data = array_merge($data, $sth->fetchAll());
+
+        var_dump($data);
     }
 
     header('HTTP/1.1 200 OK', true, 200);
